21613cba477ed716a92b2c0e88f0f126648e38e8
shiyangyang <shiyangyang@flowpp.com>
2018-03-13 20:48:51 +0800

    path of rollup

--- rollup/src/index.ts
+++ rollup/src/index.ts
@@ -3,7 +3,9 @@ import { join } from 'path'
 import { existsSync } from 'fs'
 import * as _ from 'lodash'
 
-const uglify = require('rollup-plugin-uglify')
+const UglifyJS = require('uglify-js')
+const UglifyES = require('uglify-es')
+
 const configPath = join(process.cwd(), 'rollup.config.js')
 
 export interface Options extends InputOptions {
@@ -32,16 +34,19 @@ module.exports = (conf, options) => {
             return new Promise(resolve => (async function (params) {
                 const { input, output = {}, plugins = [], ...cfg } = finalCfg;
                 const outputPath = output.file || pathname.replace(/\.\w+$/, '.js');
-                if (build) {
-                    plugins.push(uglify())
-                }
+
                 const bundle = await rollup({ ...cfg, plugins, input: pathname });
                 bundle.modules.map(({ id }) => {
-                    const d = id.replace(root, '').match(pathREG).join('/')
+                    const d = id.match(pathREG).join('/').replace(root.match(pathREG).join('/') + '/', '')
                     const p = pathname.match(pathREG).join('/')
                     _.set(imports, [d, p], 1)
                 })
                 let { code, map } = await bundle.generate(output);
+                if (build) {
+                    code = UglifyJS.minify(code).code
+                        || UglifyES.minify(code).code
+                        || code
+                }
                 if (map) {
                     store._set(outputPath + '.map', JSON.stringify(map))
                     code += `\n//# sourceMappingURL=${map.file}.map`
