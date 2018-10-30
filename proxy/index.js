21dd1718ea4631d2c0c0c4303886f91369044034
shiyangyang <shiyangyang@flowpp.com>
2018-10-30 11:56:00 +0800

    update

--- proxy/index.js
+++ proxy/index.js
@@ -10,7 +10,27 @@ module.exports = (conf, options) => {
 
     const render = function (path, req, resp) {
         if (test.test(path)) {
-            req.pipe(request(resolve(url, path.replace(test, pathname)))).pipe(resp)
+            try {
+                const newPath = resolve(url, path.replace(test, pathname))
+                const buffers = []
+                request(newPath, {
+                    method: req.method,
+                    headers: req.rawHeaders || {},
+                    body: req.rawBody || ''
+                }).on('error', function (e) {
+                    resp.writeHead(500)
+                    resp.end(e.toString())
+                }).on('data', data => {
+                    buffers.push(data)
+                }).on('response', response => {
+                    resp.writeHead(response.statusCode, response.headers)
+                }).on('end', function () {
+                    resp.end(Buffer.concat(buffers))
+                })
+            } catch (e) {
+                resp.writeHead(500)
+                resp.end(e.toString())
+            }
             return false
         }
     }
