9f7854d0f1efcb5d494a2ea7ca419cc990e48152
shiyangyang <shiyangyang@flowpp.com>
2022-03-30 17:23:18 +0800

    代理支持简单负载

--- proxy/index.js
+++ proxy/index.js
@@ -1,5 +1,5 @@
 // @ts-check
-const { resolve } = require('url')
+const { URL } = require('url')
 const http = require('http')
 module.exports = (conf, options) => {
     const {
@@ -16,6 +16,15 @@ module.exports = (conf, options) => {
     if (!url) {
         throw new Error('url needed!')
     }
+    const url_provider = (function (urls) {
+        let i = 0
+        return {
+            get: () => {
+                i = (i + 1) % urls.length
+                return urls[i]
+            }
+        }
+    })([].concat(url));
 
     /**
      * 
@@ -25,14 +34,15 @@ module.exports = (conf, options) => {
      */
     const render = function (path, req, resp) {
         if (test.test(req.url)) {
+            const _url = url_provider.get()
             try {
-                const newPath = resolve(url, req.url.replace(test, pathname))
+                const newPath = new URL(req.url.replace(test, pathname), _url)
                 debug && console.log(newPath, 'begin')
                 const creq = http.request(newPath, {
                     method: req.method,
-                    headers: Object.assign({}, renderHeaders(req), {
-                        host: url.replace(/^https?:\/\/([^\/]+).*$/, '$1')
-                    }),
+                    headers: Object.assign({}, {
+                        host: _url.replace(/^https?:\/\/([^\/]+).*$/, '$1')
+                    }, renderHeaders(req)),
                     timeout,
                 }, function (res) {
                     debug && console.log(newPath, 'response')
